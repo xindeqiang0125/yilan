6cfc3e1887d38713e98ebb1efe15d786b98c2b2f
XINDQ <1670924690@qq.com>
2018-12-22 21:09:40 +0800

    修正箭头改变起点时终点三角不随着变化的bug

--- app/src/main/java/com/xindq/yilan/view/shape/LineArrowShape.java
+++ app/src/main/java/com/xindq/yilan/view/shape/LineArrowShape.java
@@ -40,6 +40,12 @@ public class LineArrowShape extends LineShape {
     }
 
     @Override
+    public void setStart(Point start) {
+        super.setStart(start);
+        initPath();
+    }
+
+    @Override
     public void setEnd(Point end) {
         super.setEnd(end);
         initPath();
